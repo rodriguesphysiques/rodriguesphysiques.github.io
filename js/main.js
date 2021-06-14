44fa98f448a66e34504a3a0c8699778442255345
mattHydraDet <matthieu@hydradet.com>
2021-06-14 14:08:22 +0200

    Template Pages

--- js/main.js
+++ js/main.js
@@ -16,32 +16,58 @@ function scrollFunc() {
   } else if (window.pageYOffset <= sticky) {
     navbar.classList.remove("sticky");
   }
+
+  $(".adbr").css("background-position", "0%" + (scrollPosition*0.4) + "px");
 }
 
-// function startFunction() {
-//   // document.getElementById('home').focus();
-//   Scrolldown();
-//   }
+function startFunction() {
+  
+}
 
-// function Scrolldown() {
-//   window.scroll(0,300); 
-// }
+
 
 function liClicked(e) {
   var els = document.getElementsByClassName("liclck");
   
-  // for (var i = 0; i < els.length; i++) {
-    // els[i].classList.remove('liclck');
-    // }
-
-
   while(els.length > 0){
     els[0].classList.remove('liclck');
   }
   e.classList.toggle("liclck");
 }
 
+
+// JQuery only temporary
+
+function changePage(e) {
+  var menuP = e.classList;
+  menuP = menuP[menuP.length-1];
+  switch(menuP) {
+    case "homem":
+      $('#pageBody').load('home.html');
+      break;
+    case "progm":
+      $('#pageBody').load('progs.html');
+      break;
+    case "blogm":
+      $('#pageBody').load('blog.html');
+      break;
+    case "cusm":
+      $('#pageBody').load('cntcus.html');
+      break;
+  }
+}
+
+
+
+
 $(document).ready(function() {
   $('#navbar').load('navmenu.html');
   $('#pageBody').load('home.html');
+
+  // var els = document.getElementsByClassName("nvb");
+  // while(els.length > 0){
+  //   els[0].onclick = changePage(els[0]);
+
+  //   els[0].classList.remove('nvb');
+  // }
 });
